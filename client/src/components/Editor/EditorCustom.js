b9d3c3c26fb898985c96e621588c5c3945df01d6
billkondo <willkondo@gmail.com>
2018-08-11 23:35:38 -0300

    Deleted Console.log

--- client/src/components/Editor/EditorCustom.js
+++ client/src/components/Editor/EditorCustom.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
 
-export const stringifyContent = (contentState) => {
-  console.log(JSON.stringify(convertToRaw(contentState)));
-  return JSON.stringify(convertToRaw(contentState));
-}
+export const stringifyContent = (contentState) => JSON.stringify(convertToRaw(contentState));
+
 export const parseContent = (editorContent) => convertFromRaw(JSON.parse(editorContent));
