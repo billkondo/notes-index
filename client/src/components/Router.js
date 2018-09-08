eb710a2bb64a025e3e83ff04406d784e9a5ed89f
billkondo <willkondo@gmail.com>
2018-09-07 22:31:14 -0300

    Organizing Client Routers

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Page from './Page/Page';
+import NotesMenu from './Notes/Main/Menu';
+import NotesAdd from './Notes/Create/Create';
+import NotesEdit from './Notes/Edit/Edit';
+import NotesView from './Notes/View/View';
+import CollectionsMenu from './Collections/Main/Menu';
+import CollectionsAdd from './Collections/Add/Add';
 import SignIn from './SignIn/SignIn';
+
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Page} />
+      <Route exact path='/Notes' component={NotesMenu} />
+      <Route exact path='/Notes/Add' component={NotesAdd} />
+      <Route exact path='/Notes/Edit/:id' component={NotesEdit} /> 
+      <Route exact path='/Notes/View/:id' component={NotesView} />
+      <Route exact path='/Collections' component={CollectionsMenu} />
+      <Route exact path='/Collections/Add' component={CollectionsAdd} />
       <Route exact path='/SignIn' component={SignIn} />
     </Switch>
   </BrowserRouter>
