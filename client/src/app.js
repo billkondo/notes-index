2a8a725869696ce7a9894f0816ac2ee2cd6626cc
billkondo <willkondo@gmail.com>
2018-09-23 10:54:54 -0300

    Removing Draft.js

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import 'draft-js/dist/Draft.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'animate.css';
 import './styles/styles.scss';
